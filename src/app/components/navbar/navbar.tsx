b6e1651be7ea5c32b4c023c1685a52f2ef2a4757
River Stanley <river.stanley173@gmail.com>
2026-04-07 10:38:03 -0400

    Update homepage (#19)

---
+++ src/app/components/navbar/navbar.tsx
@@ -0,0 +1,11 @@
+import "./navbar.scss";
+
+export function Navigation() {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <span className="site-title">mathsoc rentals</span>
+      </div>
+    </div>
+  );
+}
