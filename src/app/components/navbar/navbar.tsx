2d838da2bb2a87da27d696c5635617849bfe50a3
River Stanley <river.stanley173@gmail.com>
2026-04-10 13:46:06 -0400

    Add page for items table

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,11 +1,50 @@
+import Link from "next/link";
 import "./navbar.scss";
 
 export function Navigation() {
   return (
     <div className="nav-container">
-      <div className="logo-container">
+      <Link href="/" className="logo-container">
         <span className="site-title">mathsoc rentals</span>
+      </Link>
+      <div className="left">
+        <NavigationDropdown title="data">
+          <NavigationDropdownItem title="items" href="/data/items" />
+        </NavigationDropdown>
       </div>
+    </div>
+  );
+}
+
+function NavigationDropdown({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="nav-dropdown">
+      <div className="nav-dropdown-button">
+        <span>{title}</span>
+      </div>
+      <div className="nav-dropdown-children">{children}</div>
+    </div>
+  );
+}
+
+function NavigationDropdownItem({
+  title,
+  href,
+}: {
+  title: string;
+  href: string;
+}) {
+  return (
+    <div className="nav-dropdown-item">
+      <Link href={href}>
+        <span>{title}</span>
+      </Link>
     </div>
   );
 }
