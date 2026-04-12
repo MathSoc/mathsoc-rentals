72a79c4a7e75762888b2ad98cb43af7f79a30755
River Stanley <river.stanley173@gmail.com>
2026-04-12 11:43:12 -0400

    Re-enable auth, enable table creation

---
+++ src/proxy.ts
@@ -0,0 +1,15 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    // Automatically authorize all requests in development
+    authorized: ({ token }) => process.env.DISABLE_AUTH === "true" || !!token,
+  },
+});
+
+export const config = {
+  matcher: ["/((?!login|api/auth|_next/static|_next/image|favicon.ico|img).*)"],
+};
