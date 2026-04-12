c6974da39a6b6543a4b1ce78ac37903d54e9c314
River Stanley <river.stanley173@gmail.com>
2026-04-12 11:45:23 -0400

    Use URL instead of path.join

--- src/app/api/util.ts
+++ src/app/api/util.ts
@@ -1,5 +1,3 @@
-import path from "path";
-
 export const workerFetch = async (route: string, options?: RequestInit) => {
   if (!process.env.WORKER_URL) {
     throw new Error("WORKER_URL unset");
@@ -8,7 +6,7 @@ export const workerFetch = async (route: string, options?: RequestInit) => {
   const headers = new Headers(options?.headers);
   headers.set("X-API-KEY", process.env.WORKER_API_KEY!);
 
-  return await fetch(path.join(process.env.WORKER_URL, route), {
+  return await fetch(new URL(process.env.WORKER_URL, route), {
     ...options,
     headers,
   });
